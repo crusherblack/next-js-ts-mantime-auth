2aeb95767b9936f9c01fe770708956ca10806159
crusherblack <fadhildarma13@gmail.com>
2023-04-30 05:47:09 +0700

    chore: add redirect

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,21 +1,18 @@
 import { ActionIcon, useMantineColorScheme } from "@mantine/core";
 import { IconSun, IconMoonStars } from "@tabler/icons-react";
+import { GetServerSidePropsContext } from "next";
 
 export default function Home() {
-  const { colorScheme, toggleColorScheme } = useMantineColorScheme();
+  const { colorScheme } = useMantineColorScheme();
   const dark = colorScheme === "dark";
 
-  return (
-    <>
-      <h1>adawd</h1>
-      <ActionIcon
-        variant="outline"
-        color={dark ? "yellow" : "blue"}
-        onClick={() => toggleColorScheme()}
-        title="Toggle color scheme"
-      >
-        {dark ? <IconSun size="1.1rem" /> : <IconMoonStars size="1.1rem" />}
-      </ActionIcon>
-    </>
-  );
+  return <></>;
 }
+export const getServerSideProps = async (ctx: GetServerSidePropsContext) => {
+  return {
+    redirect: {
+      destination: "/login",
+      permanent: false,
+    },
+  };
+};
